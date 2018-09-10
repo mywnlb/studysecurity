a8df63c9cc7f7c1996de684bf6fbda3c076934d6
mywnlb <623178903@qq.com>
2018-09-10 21:13:57 +0800

    第一次提交

---
+++ spring-security-authorize/src/main/java/com/springsecurity/lb/springsecurityauthorize/SpringSecurityAuthorizeApplication.java
@@ -0,0 +1,12 @@
+package com.springsecurity.lb.springsecurityauthorize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAuthorizeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityAuthorizeApplication.class, args);
+    }
+}
---
+++ spring-security-browse/src/main/java/com/springsecurity/lb/springsecuritybrowse/SpringSecurityBrowseApplication.java
@@ -0,0 +1,12 @@
+package com.springsecurity.lb.springsecuritybrowse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBrowseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityBrowseApplication.class, args);
+    }
+}
